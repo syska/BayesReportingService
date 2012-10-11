8fef8caa3881b2dca1cbb15928fb336314e47c0d
Mikael Syska <mikael@syska.dk>
2012-10-11 14:02:08 +0200

    Init

---
+++ src/Spam.Reporting.Service/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Spam.Reporting.Service
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var path = "." + Path.DirectorySeparatorChar;
+			if (args.Length > 0)
+				path = args[0];
+
+			var watcher = new FileSystemWatcher(path, "*.*");
+			watcher.Renamed += WatcherOnRenamed;
+			watcher.EnableRaisingEvents = true;
+			watcher.IncludeSubdirectories = true;
+			Console.WriteLine("Listening on: {0}", path);
+			Console.ReadLine();
+		}
+
+		private static void WatcherOnRenamed(object sender, RenamedEventArgs renamedEventArgs)
+		{
+			
+			Console.WriteLine("From: {0}, To: {1}", renamedEventArgs.OldFullPath, renamedEventArgs.FullPath);
+		}
+	}
+}
